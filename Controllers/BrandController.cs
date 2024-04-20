25e7f0ded0829bff1aa44c91715e174659c23759
Abdi Temesgen <99359089+hopelad@users.noreply.github.com>
2024-04-19 18:32:00 -1200

    Add files via upload
    ----------------------------------------------
    Shopping Application Using ADO.NET
    Project Overview
    - Developed a shopping application using ADO.NET for database operations.
    - Manages product listings, user accounts, and shopping cart functionality.
    
    Key Features
    Product Management
      - CRUD operations for products (add, edit, delete).
      - Categories and descriptions for products.
    User Accounts
      - User registration, login, and profile management.
      - Authentication and authorization mechanisms.
    Shopping Cart
      - Adding/removing products to/from cart.
      - Updating quantities and calculating total prices.
      - Checkout process handling.
    Technologies Used
    - ADO.NET for database connectivity.
    - ASP.NET MVC for web development.
    - C# for backend logic.
    - HTML/CSS/JavaScript for frontend.
    - SQL Server for database management.
    Implementation Details
    - Used ADO.NET's SqlConnection, SqlCommand, SqlDataReader.
    - Data validation, error handling, and exception logging.
    - Scalability and performance optimizations.
    Future Enhancements
    - Payment gateways integration.
    - Responsive design and modern UI frameworks.
    ----------------------------------------------------------
    Uploaded file contains 
    -model
    -view
    -controller
    -App_start
    and some user interface design,   so To use this source code You need to create data base and tables depending on the information detailed in the source code.

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class BrandController : Controller
+    {
+        private shopEntities db = new shopEntities();
+
+        // GET: Brand
+        public ActionResult Index()
+        {
+            return View(db.brands.ToList());
+        }
+
+        // GET: Brand/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brand brand = db.brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // GET: Brand/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Brand/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,brand1")] brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.brands.Add(brand);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+        // GET: Brand/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brand brand = db.brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brand/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,brand1")] brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brand).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+
+        // GET: Brand/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brand brand = db.brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+        // POST: Brand/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            brand brand = db.brands.Find(id);
+            db.brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
